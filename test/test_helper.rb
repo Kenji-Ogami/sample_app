7188c8297998178a6d0a2730104c6ab84c1cdafa
Kenji Ogami <ubuntu@ip-172-31-0-99.us-east-2.compute.internal>
2021-05-16 08:10:44 +0000

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,4 +16,19 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  def log_in_as(user)
+    debugger
+    session[:user_id] = user.id
+  end 
 end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password')
+    post login_path, params: { session: { email: user.email,
+                                          password: password } }
+  end
+end
+
